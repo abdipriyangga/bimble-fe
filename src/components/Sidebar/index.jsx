dcfa80b5600f1c06a5a49cce5708a1ccac6dbd58
abdipriyangga <mabdipme@gmail.com>
2022-07-15 09:47:41 +0700

    modify title dynamic

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Sidebard = () => {
+  return (
+    <>
+      <aside className='bg-main p-10 w-1/4 h-screen'>
+        <div>
+          <p className='text-white text-lg'>Dashboard</p>
+        </div>
+        <div>
+
+        </div>
+      </aside>
+    </>
+  )
+}
+
+export default Sidebard
--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Header } from '../../components/Header'
+import Sidebard from '../../components/Sidebar'
 
 const Admin = () => {
+  useEffect(() => {
+    return () => {
+      document.title = 'Admin | Bimble'
+    };
+  }, [])
   return (
     <>
-        <Header />
-        <div>Admin</div>
+      <Header />
+      <Sidebard />
     </>
   )
 }
