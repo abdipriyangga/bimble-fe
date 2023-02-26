cb318a8730a4516b315518995d7a67fcb07fdf46
Angga <60294028+abdipriyangga@users.noreply.github.com>
2023-02-26 23:23:46 +0700

    delete lonceng, route landing page, and modify margin in payment user

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,10 @@
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { ImageFirst, ImageSecond } from '../assets'
-import Footer from '../components/Footer'
-import { Header } from '../components/Header'
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
+import { ImageFirst, ImageSecond } from "../assets";
+import Footer from "../components/Footer";
+import { Header } from "../components/Header";
 
 const Homepage = () => {
   return (
@@ -12,72 +12,148 @@ const Homepage = () => {
       <header className="App-header">
         <Header name={"homepage"} />
       </header>
-      <section className='bg-main w-full h-104 flex p-5'>
-        <div className='p-2 w-1/2'>
-          <div className='text-white font-poppins font-bold mt-10'>
-            <p className='text-sm text-violet-600'>Let's Begin</p>
-            <p className='text-4xl leading-relaxed'>Back to Basic</p>
-            <p className='text-4xl leading-relaxed'>Back to Basic</p>
-            <p className='text-4xl leading-relaxed'>Back to Basic</p>
+      <section className="bg-main w-full h-104 flex p-5">
+        <div className="p-2 w-1/2">
+          <div className="text-white font-poppins font-bold mt-10">
+            <p className="text-sm text-violet-600">Let's Begin</p>
+            <p className="text-4xl leading-relaxed">Back to Basic</p>
+            <p className="text-4xl leading-relaxed">Back to Basic</p>
+            <p className="text-4xl leading-relaxed">Back to Basic</p>
           </div>
-          <p className='text-justify text-sm text-grey-400'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <p className="text-justify text-sm text-grey-400">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
         </div>
-        <div className='w-1/2 p-3'>
-          <div className='mt-10 ml-28 lg:mt-10 lg:ml-28'>
-            <img src={ImageFirst} alt="image_right_section" width={380} height={380} />
+        <div className="w-1/2 p-3">
+          <div className="mt-10 ml-28 lg:mt-10 lg:ml-28">
+            <img
+              src={ImageFirst}
+              alt="image_right_section"
+              width={380}
+              height={380}
+            />
           </div>
         </div>
       </section>
-      <section className='bg-white w-full h-104 flex p-5'>
-        <div className='w-1/2 p-3'>
-          <div className='mt-10 ml-28'>
-            <img src={ImageSecond} alt="image_left_section" width={380} height={380} />
+      <section id="about" className="bg-white w-full h-104 flex p-5">
+        <div className="w-1/2 p-3">
+          <div className="mt-10 ml-28">
+            <img
+              src={ImageSecond}
+              alt="image_left_section"
+              width={380}
+              height={380}
+            />
           </div>
         </div>
-        <div className='p-2 w-1/2'>
-          <div className='text-black font-poppins font-bold mt-10'>
-            <p className='text-sm text-grey-400'>SELECTED COURSE</p>
-            <p className='text-4xl leading-relaxed'>Back to Basic</p>
+        <div className="p-2 w-1/2">
+          <div className="text-black font-poppins font-bold mt-10">
+            <p className="text-sm text-grey-400">SELECTED COURSE</p>
+            <p className="text-4xl leading-relaxed">Back to Basic</p>
           </div>
-          <p className='text-justify text-sm mt-14 text-grey-400'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <p className="text-justify text-sm mt-14 text-grey-400">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
         </div>
       </section>
-      <section className='bg-white w-full h-104 flex p-5'>
-        <div className='p-2 w-1/2'>
-          <div className='text-black font-poppins font-bold mt-10'>
-            <p className='text-sm text-grey-400'>WHAT WE GIVE</p>
-            <p className='text-4xl leading-relaxed'>Coba Try Out Online disini</p>
+      <section id="services" className="bg-white w-full h-104 flex p-5">
+        <div className="p-2 w-1/2">
+          <div className="text-black font-poppins font-bold mt-10">
+            <p className="text-sm text-grey-400">WHAT WE GIVE</p>
+            <p className="text-4xl leading-relaxed">
+              Coba Try Out Online disini
+            </p>
           </div>
-          <p className='text-justify text-sm mt-14 text-grey-400'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <p className="text-justify text-sm mt-14 text-grey-400">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
         </div>
-        <div className='w-1/2 p-3'>
-          <div className='flex mt-10 gap-x-6 ml-28'>
-            <div className='bg-main rounded-md w-60 h-80'>
-              <div className='flex p-3'>
-                <FontAwesomeIcon icon={faUserCircle} size='3x' inverse className='mx-3' />
-                <p className='text-white text-xl mt-3 ml-5 font-semibold'>RP 10.000</p>
+        <div className="w-1/2 p-3">
+          <div className="flex mt-10 gap-x-6 ml-28">
+            <div className="bg-main rounded-md w-60 h-80">
+              <div className="flex p-3">
+                <FontAwesomeIcon
+                  icon={faUserCircle}
+                  size="3x"
+                  inverse
+                  className="mx-3"
+                />
+                <p className="text-white text-xl mt-3 ml-5 font-semibold">
+                  RP 10.000
+                </p>
               </div>
-              <div className='p-2'>
-                <p className='text-white text-xs text-center'>Pegawai Pemerintah Dengan Perjanjian Kerja (PPPK)</p>
-                <p className='text-grey-400 p-2 mt-5 text-xs text-justify'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+              <div className="p-2">
+                <p className="text-white text-xs text-center">
+                  Pegawai Pemerintah Dengan Perjanjian Kerja (PPPK)
+                </p>
+                <p className="text-grey-400 p-2 mt-5 text-xs text-justify">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s
+                </p>
               </div>
-              <div className='text-left mt-2 p-2'>
-                <Link to={`/payment-user`} type='button' className="p-2 w-32 mx-2 text-black text-center font-semibold bg-secMain border-none rounded ease-linear transition-all duration-150 shadow hover:shadow-lg hover:shadow-grey-800">
+              <div className="text-left mt-2 p-2">
+                <Link
+                  to={`/payment-user`}
+                  type="button"
+                  className="p-2 w-32 mx-2 text-black text-center font-semibold bg-secMain border-none rounded ease-linear transition-all duration-150 shadow hover:shadow-lg hover:shadow-grey-800"
+                >
                   Beli Try Out
                 </Link>
               </div>
             </div>
-            <div className='bg-main rounded-md w-60 h-80'>
-              <div className='flex p-3'>
-                <FontAwesomeIcon icon={faUserCircle} size='3x' inverse className='mx-3' />
-                <p className='text-white text-xl mt-3 ml-5 font-semibold'>RP 10.000</p>
+            <div className="bg-main rounded-md w-60 h-80">
+              <div className="flex p-3">
+                <FontAwesomeIcon
+                  icon={faUserCircle}
+                  size="3x"
+                  inverse
+                  className="mx-3"
+                />
+                <p className="text-white text-xl mt-3 ml-5 font-semibold">
+                  RP 10.000
+                </p>
               </div>
-              <div className='p-2'>
-                <p className='text-white text-xs text-center'>Calon Pegawai Negeri Sipil (CPNS)</p>
-                <p className='text-grey-400 p-2 mt-5 text-xs text-justify'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+              <div className="p-2">
+                <p className="text-white text-xs text-center">
+                  Calon Pegawai Negeri Sipil (CPNS)
+                </p>
+                <p className="text-grey-400 p-2 mt-5 text-xs text-justify">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s
+                </p>
               </div>
-              <div className='text-left mt-2 p-2'>
-                <Link to="/payment-user" type='button' className="p-2 w-32 mx-2 text-black text-center font-semibold bg-secMain border-none rounded ease-linear transition-all duration-150 shadow hover:shadow-lg hover:shadow-grey-800">
+              <div className="text-left mt-2 p-2">
+                <Link
+                  to="/payment-user"
+                  type="button"
+                  className="p-2 w-32 mx-2 text-black text-center font-semibold bg-secMain border-none rounded ease-linear transition-all duration-150 shadow hover:shadow-lg hover:shadow-grey-800"
+                >
                   Beli Try Out
                 </Link>
               </div>
@@ -87,7 +163,7 @@ const Homepage = () => {
       </section>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Logo, LogoBni, QrImage } from '../assets'
-import Footer from '../components/Footer'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Logo, LogoBni, QrImage } from "../assets";
+import Footer from "../components/Footer";
 
 const Payment = () => {
   return (
     <>
-      <div className='p-6 flex justify-between'>
-        <div className='w-1/2 flex'>
-          <img src={Logo} alt="Logo" className='mx-1' />
-          <span className='text-main my-7 mx-2 text-xl font-bold'>Back to School</span>
+      <div className="p-6 flex justify-between">
+        <div className="w-1/2 flex">
+          <img src={Logo} alt="Logo" className="mx-1" />
+          <span className="text-main my-7 mx-2 text-xl font-bold">
+            Back to School
+          </span>
         </div>
         <div>
-          <p className='text-black my-2 mx-2 text-4xl font-bold'>Pembayaran</p>
-          <p className='text-black my-2 mx-2 text-lg font-bold'>No. 001100822452</p>
+          <p className="text-black my-2 mx-2 text-4xl font-bold">Pembayaran</p>
+          <p className="text-black my-2 mx-2 text-lg font-bold">
+            No. 001100822452
+          </p>
         </div>
       </div>
-      <div className='p-10'><div className='w-full bg-grey-300 h-0.5'></div></div>
-      <div className='w-full flex justify-center mt-6'>
+      <div className="p-10">
+        <div className="w-full bg-grey-300 h-0.5"></div>
+      </div>
+      <div className="w-full flex justify-center mt-6">
         <div className="flex flex-col w-1/2 my-4">
           <div className="overflow-x-auto">
             <div className="p-1 w-full inline-block align-middle">
@@ -71,47 +77,70 @@ const Payment = () => {
           </div>
         </div>
       </div>
-      <div className='p-10'><div className='w-full bg-grey-300 h-0.5'></div></div>
-      <div className='p-12'>
-        <div className='flex justify-center gap-x-56'>
+      <div className="p-10">
+        <div className="w-full bg-grey-300 h-0.5"></div>
+      </div>
+      <div className="p-12">
+        <div className="flex justify-center gap-x-56">
           <div>
-            <div className='flex gap-x-4 flex-row'>
-              <p className='text-lg text-grey-400'>No. Rekening</p>
-              <div><img src={LogoBni} className='mt-2' alt='Logo-Bni' width={50} height={50} /></div>
+            <div className="flex gap-x-4 flex-row">
+              <p className="text-lg text-grey-400">No. Rekening</p>
+              <div>
+                <img
+                  src={LogoBni}
+                  className="mt-2"
+                  alt="Logo-Bni"
+                  width={50}
+                  height={50}
+                />
+              </div>
             </div>
-            <div className='flex gap-x-2'>
-              <p className='text-lg text-grey-500 font-bold'>2345678909</p>
-              <span className='text-lg text-grey-500 font-bold'>a.n Ari Setiawan</span>
+            <div className="flex gap-x-2">
+              <p className="text-lg text-grey-500 font-bold">2345678909</p>
+              <span className="text-lg text-grey-500 font-bold">
+                a.n Ari Setiawan
+              </span>
             </div>
           </div>
           <div>
-            <img src={QrImage} alt='Qr-logo' width={200} height={200} />
+            <img src={QrImage} alt="Qr-logo" width={200} height={200} />
           </div>
         </div>
       </div>
-      <div className='p-10'>
-        <p className='text-center text-2xl font-bold text-grey-500 uppercase'>Upload Bukti Pembayaran</p>
-        <div className='text-center mt-10'>
-          <Link to="/auth/login" type='button' className="p-2 w-72 mx-2 text-black font-semibold bg-secMain border border-secMain rounded ease-linear transition-all duration-150 shadow hover:shadow-lg">
+      <div className="p-10">
+        <p className="text-center text-2xl font-bold text-grey-500 uppercase">
+          Upload Bukti Pembayaran
+        </p>
+        <div className="text-center mt-10">
+          <Link
+            to="/auth/login"
+            type="button"
+            className="p-2 w-80 mx-2 text-black font-semibold bg-secMain border border-secMain rounded ease-linear transition-all duration-150 shadow hover:shadow-lg"
+          >
             Upload Bukti Pembayaran
           </Link>
         </div>
       </div>
-      <section className='p-10 ml-80 mb-12 w-1/2 h-1/2'>
-        <div className='p-5 border ml-36 border-grey-400 w-80 h-80'>
+      <section className="p-10 ml-[21rem] mb-12 w-1/2 h-1/2">
+        <div className="p-5 border ml-36 border-grey-400 w-80 h-80">
           <div>
-            <p className='text-lg text-grey-500 font-bold'>Upload File</p>
+            <p className="text-lg text-grey-500 font-bold">Upload File</p>
           </div>
-          <div className='w-full h-52 p-4 mt-6 border border-dashed'>
+          <div className="w-full h-52 p-4 mt-6 border border-dashed">
             <div class="md:w-2/3 lg:w-full mt-14">
-              <input class="bg-grey-200 appearance-none border-2 border-grey-200 rounded w-full py-2 px-4 text-grey-500 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="file" placeholder="URL" />
+              <input
+                class="bg-grey-200 appearance-none border-2 border-grey-200 rounded w-full py-2 px-4 text-grey-500 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                id="inline-full-name"
+                type="file"
+                placeholder="URL"
+              />
             </div>
           </div>
         </div>
       </section>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Payment
+export default Payment;
